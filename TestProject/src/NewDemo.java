d2ae4ebf363e07bd215379ddbcf25d408ae09fbf
mukund <mukund9718@gmail.com>
2023-07-26 23:04:49 +0530

    hey guys its new commit just now done

--- TestProject/src/NewDemo.java
+++ TestProject/src/NewDemo.java
@@ -3,6 +3,11 @@ public class NewDemo {
 
 	public static void main(String[] args) {
 		System.out.println("hey guys its child branch");
+		
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("sum of 2 value is:"+sum);
 
 	}
 
